4f9726bb7ec29f876f7d17ec00992b8d5234fdbd
Jamie Willis <jamster321@gmail.com>
2018-04-21 07:52:05 +0000

    Upgraded to use Scroll Phat HD

--- get_bus_times.py
+++ get_bus_times.py
@@ -1,19 +1,15 @@
-import sys
 import requests
-import datetime
 import time
-import scrollphat
+import scrollphathd
 import RPi.GPIO as GPIO
+import signal
 
-print('Pi Bus Times Started')
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-start_time = time.time()
-
-scrollphat.set_brightness(2)
-scrollphat.clear()
+REFRESH_TIMEOUT = 45.0
+SLEEP_TIMEOUT = 900.0
+LOADING_TEXT = '.'
+SCROLL_SPEED = 0.03
+APP_ID = ''
+APP_KEY = ''
 
 lines = [
     {
@@ -28,12 +24,16 @@ lines = [
     }
 ]
 
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+start_time = time.time()
+
 selected_line_index = 0;
 
-REFRESH_TIMEOUT = 45.0
-SLEEP_TIMEOUT = 600.0
-LOADING_TEXT = ' ...'
-SCROLL_SPEED = 0.07
+scrollphathd.set_brightness(0.2)
+
+print('Pi Bus Times Started')
 
 def get_selected_line():
     return lines[selected_line_index]
@@ -42,19 +42,23 @@ def seconds_to_minutes(seconds):
     return int((seconds % 3600) / 60)
 
 def get_arrival_times(stop_point, line_name):
+    print('getting arrivals')
+    arrivals = []
     try:
-        arrivals = requests.get('https://api.tfl.gov.uk/StopPoint/' + stop_point + '/arrivals').json()
+        arrivals = requests.get('https://api.tfl.gov.uk/StopPoint/' + stop_point + '/arrivals' +
+            '?app_id=' + APP_ID + '&app_key=' + APP_KEY).json()
+        arrivals = list(filter(lambda x: x['lineName'] == line_name, arrivals))
+        arrivals = list(map(lambda x: x['timeToStation'], arrivals))
+        arrivals = sorted(arrivals)
+        arrivals = list(map(lambda x: seconds_to_minutes(x), arrivals))
     except:
         print('error')
-    arrivals = list(filter(lambda x: x['lineName'] == line_name, arrivals))
-    arrivals = list(map(lambda x: x['timeToStation'], arrivals))
-    arrivals = sorted(arrivals)
-    arrivals = list(map(lambda x: seconds_to_minutes(x), arrivals))
+
     return arrivals
 
 def concat_arrival_times(arrivals):
     arrivals = list(map(lambda x: str(x) + 'min', arrivals))
-    return ' - '.join(arrivals)
+    return ' '.join(arrivals)
 
 def update_timer(timeout):
     return timeout - ((time.time() - start_time) % timeout)
@@ -63,37 +67,42 @@ def button_pressed(button):
     return button == False
 
 def scroll_display():
-    scrollphat.scroll()
+    scrollphathd.show()
+    scrollphathd.scroll()
     time.sleep(SCROLL_SPEED)
 
-def show_next_line():
-    selected_line_index = (selected_line_index + 1) % len(lines)
-
 while True:
-    scrollphat.clear()
+    scrollphathd.clear()
+    scrollphathd.show()
     if button_pressed(GPIO.input(17)):
         sleep_timer = SLEEP_TIMEOUT
+        scrollphathd.clear()
+        scrollphathd.write_string(LOADING_TEXT, 0)
+        scrollphathd.show()
 
         while int(sleep_timer) > 0:
             sleep_timer = update_timer(SLEEP_TIMEOUT)
-            refresh_timer = REFRESH_TIMEOUT
-
-            scrollphat.clear()
-            scrollphat.write_string(LOADING_TEXT, 0)
 
             selected_line = get_selected_line()
             arrival_times = get_arrival_times(selected_line['stop_point'], selected_line['line_name'])
             concat_arrivals = concat_arrival_times(arrival_times)
 
-            scrollphat.clear()
-            scrollphat.write_string(
+            refresh_timer = REFRESH_TIMEOUT
+
+            scrollphathd.clear()
+            scrollphathd.write_string(
                 ' ' + selected_line['line_name'] + 
                 ' ' + selected_line['destination'] + 
-                ' ' + concat_arrivals + ' -  ')
+                ' ' + concat_arrivals + ' -')
+            scrollphathd.show()
 
             while int(refresh_timer) > 0:
                 refresh_timer = update_timer(REFRESH_TIMEOUT)
                 scroll_display()
                 if button_pressed(GPIO.input(17)):
-                    show_next_line()
+                    scrollphathd.clear()
+                    scrollphathd.write_string(LOADING_TEXT, 0)
+                    scrollphathd.show()
+                    selected_line_index = (selected_line_index + 1) % len(lines)
+                    print('selected line' + str(selected_line_index))
                     break
